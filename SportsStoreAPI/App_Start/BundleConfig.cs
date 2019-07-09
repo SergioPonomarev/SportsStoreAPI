5879dc2690ca702f8c34b5e3e666ff1c5c17a651
SergeyPonomarev <sergiopono@gmail.com>
2019-07-09 16:24:48 +0400

    Add js files for store and _references.js file for IntelliSense. Add a bundle for store js files.

--- SportsStoreAPI/App_Start/BundleConfig.cs
+++ SportsStoreAPI/App_Start/BundleConfig.cs
@@ -13,6 +13,13 @@ namespace SportsStoreAPI
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/store").Include(
+                        "~/Scripts/Custom/storeAjax.js",
+                        "~/Scripts/Custom/storeModel.js",
+                        "~/Scripts/Custom/storeCommonController.js",
+                        "~/Scripts/Custom/storeProductsController.js",
+                        "~/Scripts/Custom/storeOrdersController.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                         "~/Content/bootstrap.css",
                         "~/Content/Site.css"));
