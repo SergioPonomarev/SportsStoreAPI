2f273a1df6badded66f66144ed8fb263db077cad
SergeyPonomarev <sergiopono@gmail.com>
2019-07-09 16:36:58 +0400

    storeAjax implementation.

--- SportsStoreAPI/Scripts/Custom/storeAjax.js
+++ SportsStoreAPI/Scripts/Custom/storeAjax.js
@@ -1,1 +1,32 @@
-﻿
+﻿var sendRequest = function (url, verb, data, successCallback, errorCallback, options) {
+    var requestOptions = options || {};
+    requestOptions.type = verb;
+    requestOptions.success = successCallback;
+    requestOptions.error = errorCallback;
+
+    if (!url || !verb) {
+        errorCallback(401, "Url and HTTP verb required");
+    }
+
+    if (data) {
+        requestOptions.data = data;
+    }
+
+    $.ajax(url, requestOptions);
+}
+
+var setDefaultCallbacks = function (successCallback, errorCallback) {
+    $.ajaxSetup({
+        complete: function (jqXHR, status) {
+            if (jqXHR.status >= 200 && jqXHR.status < 300) {
+                successCallback(jqXHR.responseJSON);
+            } else {
+                errorCallback(jqXHR.status, jqXHR.statusText);
+            }
+        }
+    });
+}
+
+var setAjaxHeaders = function (requestHeaders) {
+    $.ajaxSetup({ headers: requestHeaders });
+}
